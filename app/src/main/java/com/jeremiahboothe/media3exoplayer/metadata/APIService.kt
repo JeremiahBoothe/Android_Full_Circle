ffeb9fa128fba7f2786055652466c15f138e497a
jeremiah <jeremiahboothe@outlook.com>
2024-09-07 02:54:21 -0400

    Barely working baseline, Notification controls and lock screen controls no longer work in Android 14, need to overhaul for modern android.

--- app/src/main/java/com/jeremiahboothe/media3exoplayer/metadata/APIService.kt
+++
@@ -1,9 +0,0 @@
-package com.jeremiahboothe.media3exoplayer.metadata
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface APIService {
-    @GET("api/stations/umg0tz9sywzuv/now_playing/")
-    suspend fun getMetaData(): Response<SimpleJSONModel>
-}
